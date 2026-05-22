f5722b726430cbf83f439b469ed1ac23423cfc41
Katie Lam <katie.lam102@gmail.com>
2026-05-22 05:46:17 -0400

    basic file reading

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+file = st.file_uploader("Upload dataset here:", type="csv", accept_multiple_files=False, width="stretch""")
+
+if file is not None:
+    st.write("Uploaded file successfully!")
+    st.write("Data:")
+    df = pd.read_csv(file)
+    df
